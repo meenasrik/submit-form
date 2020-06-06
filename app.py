0911c37030fc2626cbc57a605932c41e277e17b5
srimee <junnamma@yahoo.com>
2020-06-06 16:51:28 -0400

    Inserting user input values into the database

--- app.py
+++ app.py
@@ -13,13 +13,33 @@ app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 
+# instantiate an object for the MySQL module
+mysql = MySQL(app)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == "POST":
 		name = request.form['uname']
 		age = request.form['uage']
-		val = "Registered user " + name + " of age " + age
-		return val
+		
+		# create a cursor by executing cursor function of db object
+		cur = mysql.connection.cursor()
+		
+		query = "SELECT * FROM table_name WHERE name='" + name + "'"
+		count = cur.execute(query)
+		if count > 0:
+			message = "That name already exists. Choose another one."
+			return render_template("index.html", message = message, title="Form")
+		else:
+			# take cursor object and call execute function to execute query
+			cur.execute("INSERT INTO table_name(name, age) VALUES(%s, %s)", (name, age))
+
+			# commit the changes to the database
+			mysql.connection.commit()
+
+			# cleanup
+			cur.close()
+		
 	return render_template("index.html", title="Form")
 
 if __name__ == '__main__':
